89f779664e927da835795300e37be7f5ee32e239
Ing. Daniel Torres Ruiz <torresruizdaniel23@gmail.com>
2022-08-14 14:00:52 -0500

    RULE-003: NavBar refactoring styles
    RULE-003: NavBar component refactoring styles

--- src/pages/Landing/components/NavBar/index.jsx
+++ src/pages/Landing/components/NavBar/index.jsx
@@ -1,10 +1,16 @@
 import search from 'assets/img/search.svg';
 import { Nav, DivContainer, } from 'views/Tags/BlockLevel'
 import { H1 } from 'views/Tags/Text';
-import { Button } from 'views/Tags/Form/Action';
 import { Input } from 'views/Tags/Form/Inputs';
 import { Image } from 'views/Tags/Picture';
 import { useStyles } from './styles';
+import styled from 'styled-components';
+
+const HamburgerButton = styled.button`
+    @media all and (min-width: 1100px) {
+        display: none;
+    }
+`
 
 export const NavBar = () => {
     const classes = useStyles();
@@ -13,9 +19,9 @@ export const NavBar = () => {
         <Nav className={classNav} role="navigation">
             <DivContainer className="max-centered">
                 <H1 className="nav__logo">Rule of thumb.</H1>
-                <Button className="nav__hamburger icon-button" alt="Open Menu">
+                <HamburgerButton className="nav__hamburger icon-button" alt="Open Menu">
                     <svg width="25" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h25v4H0V0zm0 8h25v4H0V8zm0 8h25v4H0v-4z" fill="#FFF" fill-rule="nonzero"/></svg>
-                </Button>
+                </HamburgerButton>
                 <ul className="nav__links">
                     <li>
                         <a href="#">Past Trials</a>
@@ -29,9 +35,9 @@ export const NavBar = () => {
                     <li>
                         <form>
                             <Input className="nav__search-input" aria-label="search" type="text" />
-                            <Button className="nav__search icon-button" alt="Search" type="submit">
+                            <button className="nav__search icon-button" alt="Search" type="submit">
                                 <Image src={search} alt="search" />
-                            </Button>
+                            </button>
                         </form>
                     </li>
                 </ul>
