372e3761fccbd9da89bd0ef40dd2b690a23e520f
Ing. Daniel Torres Ruiz <torresruizdaniel23@gmail.com>
2022-08-13 22:17:54 -0500

    RULE-002: Create Wrapped tags with styled components
    RULE-002: Create components as Wrapped to maintain syntactic convention

---
+++ src/views/Tags/BlockLevel/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const DivContainer = styled.div``
+
+export const Header = styled.header``
+
+export const Main = styled.main``
+
+export const Footer = styled.footer``
+
+export const Article = styled.article``
+
+export const Section = styled.section``
---
+++ src/views/Tags/Text/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Paragraph = styled.p``
+
+export const CustomParagraph = styled.p`
+    font-size: ${(props) => props.$fontSize};
+`
+export const H2 = styled.h2``
